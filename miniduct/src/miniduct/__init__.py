49da73499c398b543d29e1c1733ec507b7b99086
watacoso <watacoso@gmail.com>
2025-07-03 13:04:18 +0000

    fix

--- miniduct/src/miniduct/__init__.py
+++ miniduct/src/miniduct/__init__.py
@@ -1,3 +1,3 @@
-from miniduct.pipeline import run_pipeline
 from miniduct.datasources import DataSource, HttpDataSource, PythonDataSource
-from miniduct.outputs import Output, GCSOutput
+from miniduct.outputs import GCSOutput, Output
+from miniduct.pipeline import run_pipeline
